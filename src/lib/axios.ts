a160cd3662cc9d69c258ebb28c9400e0ae2f8480
tushkarmakar-coder <tushkarmakar@gmail.com>
2026-08-14 17:02:55 +0530

    Fix hardcoded backend API URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:3000/api',
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
